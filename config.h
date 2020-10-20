bddcadfb3b1a3d10ba31d5b9a2a2b7d26845395a
cycleke <cycleke@gmail.com>
2020-10-20 17:07:52 +0800

    修改字体

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Monaco:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 9;
 
 /*
